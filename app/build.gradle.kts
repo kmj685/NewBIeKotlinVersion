46f0b9a810b983e45d1d8e5fb4161ea502fcb9f8
nohyeongteak <yt873588@gmail.com>
2026-07-24 22:34:46 +0900

    FIX/ local.properties api anon 키 바뀜

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.newBie.new_bie"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 9
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
